59c4d4fbe5ccf5933945ade028857f7688f84f01
manjula <manjula@bitcot.com>
2017-04-22 18:07:45 +0530

    group edit

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -17,6 +17,22 @@ class Admin::GroupsController < Admin::AdminApplicationController
     redirect_to admin_groups_path
   end
 
+  def edit
+    @group=Group.find(params[:id])
+    @promo_reps=@current_client.users.where(role: 'promo_rep')
+  end
+
+  def update
+    @group=Group.find(params[:id])
+    if @group.update_attributes(group_params)
+      @group.update(user_ids: params[:group][:user_ids]) if params[:group][:user_ids]
+      redirect_to admin_groups_path
+    else
+      flash[:error]=@group.errors.full_messages.join(', ')
+      redirect_to :back
+    end
+  end
+
 
   private
   def group_params
