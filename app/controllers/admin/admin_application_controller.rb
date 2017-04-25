893fab0ad6a0cb7114b2d0119a99736b58625a9f
manjula <manjula@bitcot.com>
2017-04-25 16:38:32 +0530

    set current client

--- app/controllers/admin/admin_application_controller.rb
+++ app/controllers/admin/admin_application_controller.rb
@@ -8,12 +8,27 @@ module Admin
     before_action :set_client
 
     def set_client
-      @current_client = current_user.client
+      @current_client = Client.where(:admin_id => current_user.id).first
     end
 
     def authenticate_admin
       if current_user.client_admin? and !current_user.client_id.nil?
         true
+      end
+    end
+
+    def login_as_master
+      if session[:slave_user_id].blank?
+        flash[:error] = 'Sorry! Unable to process!'
+        redirect_back fallback_location: superadmin_clients_path
+      else
+        if session[:slave_user_id] == current_user.id
+          session[:slave_user_id] = nil
+          redirect_to admin_events_path
+        else
+          flash[:error] = 'Sorry! Unable to process!'
+          redirect_back fallback_location: superadmin_clients_path
+        end
       end
     end
   end
