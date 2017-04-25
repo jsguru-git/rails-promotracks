09068116136e9513e435e6992cb9520c63f87a4b
priya <hema@bitcot.com>
2017-04-25 17:13:57 +0530

    route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations", sessions: "sessions"}
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   root 'homes#index'
 
   resources :homes
