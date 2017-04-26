755b9d446d9868932e6a45e344fc500973b4dcf2
priya <hema@bitcot.com>
2017-04-26 13:44:25 +0530

    Login Page Improvements

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
+Rails.application.config.assets.paths << "vendor/assets/cleanui-admin-template/js"
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
+Rails.application.config.assets.paths << "vendor/assets/cleanui-admin-template/stylesheets"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/ #fonts
