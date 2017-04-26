36fe39f36347a5f81300edc7c23451dc3cb8c6ec
manjula <manjula@bitcot.com>
2017-04-26 23:40:44 +0530

    commented seeds by mistake

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,38 +38,38 @@ end
     # email_data[:user]=promo_ref_info(promo_rep)
     # UserMailer.send_email(promo_rep.email, email_data).deliver
   end
-  #
-  # 6.times do |k|
-  #   group=client.groups.new(:name => Faker::Name.name)
-  #   group.user_ids = user_ids
-  #   group.save
-  # end
-  # 6.times do |l|
-  #   rep_event=client.events.new(:promo_category => 'promo_rep', :name => Faker::Name.name, :event_type_id => event_type, :end_time => Faker::Time.forward(23, :morning), :start_time => Faker::Time.backward(14, :evening), :brand_id => brand)
-  #   rep_event.creator = client.admin
-  #   user_ids.each do |id|
-  #     rep_event.user_events.new(user_id: id, token: SecureRandom.hex[0, 6], status: :accepted)
-  #   end
-  #   rep_event.address=Location.new(:city => 'San Diego')
-  #   rep_event.save
-  # end
-  #
-  #
-  # 6.times do |l|
-  #   rep_event=client.events.new(:promo_category => 'promo_group', :name => Faker::Name.name, :event_type_id => event_type, :end_time => Faker::Time.forward(23, :morning), :start_time => Faker::Time.backward(14, :evening), :brand_id => brand, :max_users => 2, :group_id => group_id)
-  #   rep_event.creator = client.admin
-  #   rep_event.address=Location.new(:city => 'San Diego')
-  #   rep_event.save
-  #   # email_data={}
-  #   # email_data[:body] = "Please find below the event details"
-  #   # email_data[:subject]="#{rep_event.name} :#{rep_event.id}"
-  #   # email_data[:event]=get_event(rep_event)
-  #   unless rep_event.group_id.nil?
-  #     rep_event.group.users.each do |user|
-  #       token=SecureRandom.hex[0, 6]
-  #       rep_event.user_events.create(user_id: user.id, token: token, category: :promo_group)
-  #       # EventMailer.accept_event(user.email, email_data, token).deliver
-  #     end
-  #   end
-  # end
+
+  6.times do |k|
+    group=client.groups.new(:name => Faker::Name.name)
+    group.user_ids = user_ids
+    group.save
+  end
+  6.times do |l|
+    rep_event=client.events.new(:promo_category => 'promo_rep', :name => Faker::Name.name, :event_type_id => event_type, :end_time => Faker::Time.forward(23, :morning), :start_time => Faker::Time.backward(14, :evening), :brand_id => brand)
+    rep_event.creator = client.admin
+    user_ids.each do |id|
+      rep_event.user_events.new(user_id: id, token: SecureRandom.hex[0, 6], status: :accepted)
+    end
+    rep_event.address=Location.new(:city => 'San Diego')
+    rep_event.save
+  end
+
+
+  6.times do |l|
+    rep_event=client.events.new(:promo_category => 'promo_group', :name => Faker::Name.name, :event_type_id => event_type, :end_time => Faker::Time.forward(23, :morning), :start_time => Faker::Time.backward(14, :evening), :brand_id => brand, :max_users => 2, :group_id => group_id)
+    rep_event.creator = client.admin
+    rep_event.address=Location.new(:city => 'San Diego')
+    rep_event.save
+    # email_data={}
+    # email_data[:body] = "Please find below the event details"
+    # email_data[:subject]="#{rep_event.name} :#{rep_event.id}"
+    # email_data[:event]=get_event(rep_event)
+    unless rep_event.group_id.nil?
+      rep_event.group.users.each do |user|
+        token=SecureRandom.hex[0, 6]
+        rep_event.user_events.create(user_id: user.id, token: token, category: :promo_group)
+        # EventMailer.accept_event(user.email, email_data, token).deliver
+      end
+    end
+  end
 end
