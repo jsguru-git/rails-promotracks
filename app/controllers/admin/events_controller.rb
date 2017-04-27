e7980118358887e440c2fe9b5273b9224bd43199
manjula <manjula@bitcot.com>
2017-04-27 22:18:58 +0530

    events update call changes

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -64,22 +64,52 @@ class Admin::EventsController < Admin::AdminApplicationController
 
   def update
     @event=Event.find(params[:id])
-    if event_params[:promo_category]=='promo_rep' and @event.promo_category_was=='promo_rep'
-      unless params[:event][:user_ids].delete_if { |x| x.empty? }.nil?
-        delete_users=@event.user_ids-(params[:event][:user_ids].map(&:to_i))
-        delete_users.each do |del_user|
-          @event.user_events.where(user_id: del_user).first.delete
+    if @event.update_attributes(event_params.except(:promo_category))
+      @event.update_attribute(:start_time, DateTime.strptime(event_params[:start_time], '%m/%d/%Y %I:%M %p')) unless event_params[:start_time].nil?
+      @event.update_attribute(:end_time, DateTime.strptime(event_params[:end_time], '%m/%d/%Y %I:%M %p')) unless event_params[:end_time].nil?
+      if event_params[:promo_category]=='promo_rep' and @event.promo_category_was=='promo_rep'
+        unless params[:event][:user_ids].delete_if { |x| x.empty? }.nil?
+          @event.update_attribute(:promo_category, :promo_rep)
+          delete_users=@event.user_ids-(params[:event][:user_ids].map(&:to_i))
+          delete_users.each do |del_user|
+            @event.user_events.where(user_id: del_user).first.delete
+          end
+          create_users=(params[:event][:user_ids].map(&:to_i))-delete_users
+          create_users.each do |user_id|
+            if @event.user_ids.exclude? user_id
+              @event.user_events.create(user_id: user_id, token: SecureRandom.hex[0, 6], status: :accepted)
+            end
+          end
         end
-        create_users=(params[:event][:user_ids].map(&:to_i))-delete_users
-        create_users.each do |user_id|
-          if @event.user_ids.exclude? user_id
+      elsif event_params[:promo_category]=='promo_group' and @event.promo_category_was=='promo_group'
+        unless event_params[:group_id].nil?
+          @event.update_attribute(:promo_category, :promo_group)
+          if @event.group_id_was!=event_params[:group_id].to_i
+            UserEvent.where(:event_id => @event.id).delete_all
+            @event.update_attribute(:group_id, event_params[:group_id])
+            email_data={}
+            email_data[:body] = "Please find below the event details"
+            email_data[:subject]="#{@event.name} :#{@event.id}"
+            email_data[:event]=get_event(@event)
+            @event.group.users.each do |user|
+              token=SecureRandom.hex[0, 6]
+              @event.user_events.create(user_id: user.id, token: token, category: :promo_group)
+              EventMailer.accept_event(user.email, email_data, token).deliver
+            end
+          end
+        end
+      elsif event_params[:promo_category]=='promo_rep' and @event.promo_category_was=='promo_group'
+        @event.update_attribute(:group_id, nil)
+        UserEvent.where(:event_id => @event.id).delete_all
+        @event.update_attribute(:promo_category, :promo_rep)
+        unless params[:event][:user_ids].delete_if { |x| x.empty? }.nil?
+          params[:event][:user_ids].delete_if { |x| x.empty? }.each do |user_id|
             @event.user_events.create(user_id: user_id, token: SecureRandom.hex[0, 6], status: :accepted)
           end
         end
-      end
-    elsif event_params[:promo_category]=='promo_group' and @event.promo_category_was=='promo_group'
-      unless event_params[:group_id].nil?
-        if @event.group_id_was!=event_params[:group_id].to_i
+      elsif event_params[:promo_category]=='promo_group' and @event.promo_category_was=='promo_rep'
+        unless event_params[:group_id].nil?
+          @event.update_attribute(:promo_category, :promo_group)
           UserEvent.where(:event_id => @event.id).delete_all
           @event.update_attribute(:group_id, event_params[:group_id])
           email_data={}
@@ -93,32 +123,6 @@ class Admin::EventsController < Admin::AdminApplicationController
           end
         end
       end
-    elsif event_params[:promo_category]=='promo_rep' and @event.promo_category_was=='promo_group'
-      @event.update_attribute(:group_id, nil)
-      UserEvent.where(:event_id => @event.id).delete_all
-      unless params[:event][:user_ids].delete_if { |x| x.empty? }.nil?
-        params[:event][:user_ids].delete_if { |x| x.empty? }.each do |user_id|
-          @event.user_events.create(user_id: user_id, token: SecureRandom.hex[0, 6], status: :accepted)
-        end
-      end
-    elsif event_params[:promo_category]=='promo_group' and @event.promo_category_was=='promo_rep'
-      unless event_params[:group_id].nil?
-        UserEvent.where(:event_id => @event.id).delete_all
-        @event.update_attribute(:group_id, event_params[:group_id])
-        email_data={}
-        email_data[:body] = "Please find below the event details"
-        email_data[:subject]="#{@event.name} :#{@event.id}"
-        email_data[:event]=get_event(@event)
-        @event.group.users.each do |user|
-          token=SecureRandom.hex[0, 6]
-          @event.user_events.create(user_id: user.id, token: token, category: :promo_group)
-          EventMailer.accept_event(user.email, email_data, token).deliver
-        end
-      end
-    end
-    if @event.update_attributes(event_params)
-      @event.update_attribute(:start_time, DateTime.strptime(event_params[:start_time], '%m/%d/%Y %I:%M %p')) unless event_params[:start_time].nil?
-      @event.update_attribute(:end_time, DateTime.strptime(event_params[:end_time], '%m/%d/%Y %I:%M %p')) unless event_params[:end_time].nil?
       redirect_to admin_events_path
     else
       flash[:error]= @event.errors.full_messages.join(',')
