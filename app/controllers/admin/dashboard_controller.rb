9f979a32a79a3c77339188cc98e946c9296508fa
manjula <manjula@bitcot.com>
2017-04-27 18:34:29 +0530

    dashboard changes

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,23 @@ class Admin::DashboardController < Admin::AdminApplicationController
 
   def index
     @events=@current_client.events.order("created_at asc").limit(2)
+    @total=0
+    @total_attendance=0
+    @total_sample=0
+    @events.each do |event|
+      if event.user_events.collect { |c| c[:total_expense] }.any?
+        @total=event.user_events.collect { |c| c[:total_expense] }.reduce(0, :+)
+        @total+=@total
+      end
+      if event.user_events.collect { |c| c[:attendance] }.any?
+        @total_attendance=event.user_events.collect { |c| c[:attendance] }.reduce(0, :+)
+        @total_attendance+=@total_attendance
+      end
+      if event.user_events.collect { |c| c[:sample] }.any?
+        @total_sample=event.user_events.collect { |c| c[:sample] }.reduce(0, :+)
+        @total_sample+=@total_sample
+      end
+
+    end
   end
 end
