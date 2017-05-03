8580e6b6a6d59a760295ecb6d0767b639750c593
manjula <manjula@bitcot.com>
2017-05-03 12:15:29 +0530

    save sample,attendance

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -47,12 +47,10 @@ class Api::V1::EventsController < Api::V1::ApiApplicationController
   end
 
   private
-  def event_params
-    params.require(:event).permit(:attendance, :sample)
-  end
+
 
   def user_event_params
-    params.require(:user_event).permit(:notes, :total_expense, :check_in, :check_out, :images, :follow_up, :recommended)
+    params.require(:user_event).permit(:notes, :total_expense, :check_in, :check_out, :images, :follow_up, :recommended, :attendance, :sample)
   end
 
   def add_images(images, event)
