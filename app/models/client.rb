7bc1d6997ac7e63b0ee9fe936917c1310381fbf2
manjula <manjula@bitcot.com>
2017-05-03 11:49:00 +0530

    commenting phone validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,5 +5,5 @@ class Client < ActiveRecord::Base
   has_and_belongs_to_many :brands, join_table: :client_brands
   belongs_to :admin, class_name: 'User', foreign_key: :admin_id
   accepts_nested_attributes_for :admin, reject_if: :all_blank, allow_destroy: true
-  validates_length_of :phone, :minimum => 15, :maximum => 15
+  # validates_length_of :phone, :minimum => 14, :maximum => 14
 end
