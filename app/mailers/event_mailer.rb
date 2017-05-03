373771a99e82e92fdea2f5aa14d6655e94b2df12
manjula <manjula@bitcot.com>
2017-05-03 22:14:20 +0530

    final fix for group accept

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,9 +2,7 @@ class EventMailer < ApplicationMailer
 
   def accept_event(to_email, data, token=nil)
     @data = data
-    Rails.logger.info "#{root_url}"
-    Rails.logger.info "#{event_url(:id => data[:event][:id], :token => token)}"
-    @data[:url]= "#{root_url}/events/#{data[:event][:id]}?token=#{token}"
+    @data[:token]= token
     mail(:to => to_email, :subject => @data[:subject])
   end
 
