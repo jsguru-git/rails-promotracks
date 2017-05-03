a3e2e588db6dd5af26017edaa9c6960dd98423ca
manjula <manjula@bitcot.com>
2017-05-03 21:54:35 +0530

    group accept event changes

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,7 +2,7 @@ class EventMailer < ApplicationMailer
 
   def accept_event(to_email, data, token=nil)
     @data = data
-    @data[:token]= token
+    @data[:url]= event_url(:id => data[:event][:id], :token => token)
     mail(:to => to_email, :subject => @data[:subject])
   end
 
