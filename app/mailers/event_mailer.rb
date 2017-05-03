6d90366f902a73dd541b652130ea96e51b705753
manjula <manjula@bitcot.com>
2017-05-03 22:09:54 +0530

    logger info for url

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,7 +2,9 @@ class EventMailer < ApplicationMailer
 
   def accept_event(to_email, data, token=nil)
     @data = data
-    @data[:url]= event_url(:id => data[:event][:id], :token => token)
+    Rails.logger.info "#{root_url}"
+    Rails.logger.info "#{event_url(:id => data[:event][:id], :token => token)}"
+    @data[:url]= "#{root_url}/events/#{data[:event][:id]}?token=#{token}"
     mail(:to => to_email, :subject => @data[:subject])
   end
 
