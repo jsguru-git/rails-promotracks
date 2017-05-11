bf14da5dba8c7a616af4ded69d592d3fe6f42ec5
manjula <manjula@bitcot.com>
2017-05-11 12:32:01 +0530

    dashboard bug fixes and event brand mandatory

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -15,24 +15,26 @@ class Admin::DashboardController < Admin::AdminApplicationController
     @events.each do |event|
 
       active_events=event.user_events.where(:status => UserEvent::statuses[:accepted])
+      unless active_events.empty?
+        total_expense=active_events.collect { |c| c[:total_expense] }.compact.reduce(0, :+)
+        total << total_expense
 
-      total_expense=active_events.collect { |c| c[:total_expense] }.compact.reduce(0, :+)
-      total << total_expense
+        total_attendance=active_events.collect { |c| c[:attendance] }.compact.reduce(0, :+)
+        attendance << total_attendance
 
-      total_attendance=active_events.collect { |c| c[:attendance] }.compact.reduce(0, :+)
-      attendance << total_attendance
+        total_sample=active_events.collect { |c| c[:sample] }.compact.reduce(0, :+)
+        sample << total_sample
 
-      total_sample=active_events.collect { |c| c[:sample] }.compact.reduce(0, :+)
-      sample << total_sample
+        total_product_cost=active_events.collect { |c| (c[:sample]||0)* (event.brand&.unit_cost||0) }
+        product_cost<<total_product_cost
 
-      total_product_cost=active_events.where.not(:sample => nil).collect { |c| c[:sample]* event.brand.unit_cost }
-      product_cost<<total_product_cost
-
-      active_events.where.not(:check_in=>nil,:check_out=>nil).each do |active|
-        hour=((active.check_out - active.check_in)/3600).round
-        pay=hour*event.pay
-        final_pay << pay
+        active_events.where.not(:check_in => nil, :check_out => nil).each do |active|
+          hour=((active.check_out - active.check_in)/3600).round
+          pay=hour*event.pay
+          final_pay << pay
+        end
       end
+
     end
     @total_attendance=attendance.collect { |c| c }.compact.reduce(0, :+)
     @total_sample=sample.collect { |c| c }.compact.reduce(0, :+)
