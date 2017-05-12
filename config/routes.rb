6fe3e4c600d022f20ef92153bed4c92acefe97b0
manjula <manjula@bitcot.com>
2017-05-12 10:17:24 +0530

    routes for sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
   devise_for :users, controllers: {registrations: "registrations", sessions: "sessions", invitations: "invitations"}
   devise_scope :user do
     root to: 'devise/sessions#new'
