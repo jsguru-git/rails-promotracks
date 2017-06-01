2f466046c3ae26aa56637a5b13b9c3d80d37e0b1
manjula <manjula@bitcot.com>
2017-06-01 12:27:59 +0530

    user event images

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -276,5 +276,10 @@ $(document).on('turbolinks:load', function () {
         });
     });
 
+    $('.pop').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+
 
 });
