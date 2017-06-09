3c05a42b879b7fe5084cc178b6dafed2e6c4ead4
manjula <manjula@bitcot.com>
2017-06-09 10:56:30 +0530

    seeds for production superadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create(email: 'dev@bitcot.com', password: 'bitcotadmin', password_confirmation: 'bitcotadmin', first_name: 'Dev', last_name: 'Bitcot', role: :super_admin)
-# User.create(email: 'admin@promotracks.com', password: 'iampromo', password_confirmation: 'iampromo', first_name: 'Promo', last_name: 'user', role: :super_admin)
+# User.create(email: 'dev@bitcot.com', password: 'bitcotadmin', password_confirmation: 'bitcotadmin', first_name: 'Dev', last_name: 'Bitcot', role: :super_admin)
+User.create(email: 'admin@promotracks.com', password: 'Diego&Promo!445$', password_confirmation: 'Diego&Promo!445$', first_name: 'Promo', last_name: 'user', role: :super_admin)
